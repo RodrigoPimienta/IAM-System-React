3f86bcd73f3deff4db5d47351bb6d8bddf64c93e
RodrigoPimienta <capitanpimienta28@gmail.com>
2025-01-15 14:46:47 -0600

    feat(Components): :sparkles: New component: CustomTable
    Allows to create table suing columns, rows and a custom component for the actions

---
+++ src/components/CustomTable.jsx
@@ -0,0 +1,24 @@
+export const CustomTable = ({ columns, rows, ActionsComponent }) => {
+    return (
+        <table>
+            <thead>
+                <tr>
+                    {columns.map((column, index) => (
+                        <th key={index}>{column.header}</th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                {rows.map((row, rowIndex) => (
+                    <tr key={rowIndex}>
+                        {columns.map((column, colIndex) => (
+                            <td key={colIndex}>
+                                {column.render ? column.render(row, ActionsComponent) : row[column.key] || '-'}
+                            </td>
+                        ))}
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
