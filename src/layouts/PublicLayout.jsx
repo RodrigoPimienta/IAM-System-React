1ac773cdcad8b4fd8cb3ce97949d8c8b263524ea
RodrigoPimienta <capitanpimienta28@gmail.com>
2025-01-16 02:26:02 -0600

    feat(Layaouts): :sparkles: Public layaout

--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -4,11 +4,19 @@ export const PublicLayout = () => {
   return (
     <div>
       <header>
-        <h1>Bienvenido a IAM System</h1>
+        <h1>Bienvenido a Pimi IAM System</h1>
       </header>
       <main>
         <Outlet /> {/* Aquí se renderizan las rutas hijas del PublicLayout */}
       </main>
+      <footer>
+        <nav>
+          <ul>
+            <li><a href="www.linkedin.com/in/rodigopimienta" target="_blank">LinkedIn</a></li>
+            <li><a href="https://github.com/tu-repo-del-proyecto" target="_blank">Repositorio del Proyecto</a></li>
+          </ul>
+        </nav>
+      </footer>
     </div>
   );
 };
