bdf781e4b4056ade3b0377a7666fcd1a7ea7326c
RodrigoPimienta <capitanpimienta28@gmail.com>
2025-01-16 06:22:36 -0600

    feat(Layaouts): :sparkles: New layour: ProtectedRoute
    Verify if the user is login to give acces to the private part of the app

--- src/layouts/ProtectedRoute .jsx
+++ src/layouts/ProtectedRoute .jsx
@@ -1,7 +1,8 @@
 import { Navigate } from "react-router";
+import { useAuth } from "../hooks/useAuth";
 
 export const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = true; // Cambia esto según tu lógica de autenticación
-
+  const { auth } = useAuth();
+  const isAuthenticated = auth.isLoggedIn;
   return isAuthenticated ? children : <Navigate to="/" />;
 };
