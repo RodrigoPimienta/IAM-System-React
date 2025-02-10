c7d8b5350d3f1d01c91fbd184ece4c6d5d52685f
RodrigoPimienta <capitanpimienta28@gmail.com>
2025-02-10 05:25:17 -0600

    refactor(Pages): :recycle: AddUser: use new custom hook users
    Handle succes and error on addUser

--- src/features/users/pages/AddUser.jsx
+++ src/features/users/pages/AddUser.jsx
@@ -1,35 +1,59 @@
 import { useNavigate } from "react-router";
-import { CustomForm } from "../../../components/CustomForm";
 import { usePermissions } from "../../../hooks/usePermissions";
+import { useUsers } from "../hooks/useUsers";
+import { Loading, Error , CustomForm} from "../../../components";
+import Swal from "sweetalert2";
+
 
 const moduleKey = "users";
 
 export const AddUser = () => {
+    const navigate = useNavigate();
     const { permissions } = usePermissions();
     const permissionsPage = permissions[moduleKey]?.permissions || {};
-    const navigate = useNavigate();
-  
+    const {postUser, isLoading} = useUsers();
+
     const handleAddUser = (formData) => {
-      console.log("User added:", formData);
-      // Aquí puedes enviar los datos a la API con fetch o axios
-    };
+      postUser.mutate(formData, {
+          onSuccess: () => {
+              Swal.fire({
+                  icon: "success",
+                  title:"Success",
+                  text: "User added successfully",
+                  showConfirmButton: true,
+                  timer: 4000,
+              }).then(() => {
+                  navigate("/admin/users");
+              });
+          },
+          onError: (err) => {
+              Swal.fire({
+                  icon: "error",
+                  title: "Error",
+                  text: err.message,
+              });
+          },
+      });
+  };
   
+
     return (
       <>
+       {isLoading && <Loading />}
         <CustomForm
           title="Add User"
           permissionsPage={permissionsPage}
           actionsHeader={[]}
           fields={[
             { key: "name", input: "input", type: "text", label: "Name", placeholder: "Enter name", required: true },
-            { key: "email", input: "input", type: "email", label: "Email", placeholder: "Enter email", required: true },
+            { key: "email", input: "input", type: "email", label: "Email", placeholder: "Enter email", required: false },
             { key: "password", input: "input", type: "password", label: "Password", placeholder: "Enter password", required: true },
             { key: "password_confirmation", input: "input", type: "password", label: "Password confirmation", placeholder: "Enter password", required: true, validation: "same:password" },
             {
               key: "profile",
               input: "select",
               label: "Profile",
-              required: true,
+              required: false,
               init: "Select profile",
               options: [
                 { value: "1", label: "Admin" },
