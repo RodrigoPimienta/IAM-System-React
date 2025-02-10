415c27ceaee0946c269a57a8c8c8643e780c089a
RodrigoPimienta <capitanpimienta28@gmail.com>
2025-02-10 00:05:37 -0600

    feat(Components): :sparkles: Error: Component to fire a sweetAlert

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,5 +1,5 @@
 
-export const Error = ({icon, tittle, text}) => {
+export const Error = ({icon = 'error', tittle ='Error', text}) => {
   // regresar un sweetalert con el icono, titulo y texto
   return (
     Swal.fire({
