a45559e941950c58eebf4882f68834ae41a28a1a
RodrigoPimienta <capitanpimienta28@gmail.com>
2025-02-25 22:11:51 -0600

    feat(Hooks): :sparkles: Modules: get, post, edit, updateStatus

--- src/features/modules/hooks/useModules.js
+++ src/features/modules/hooks/useModules.js
@@ -1,47 +1,78 @@
-import { useState } from "react"
-import {getModulesAPI} from '../services/modules'
+import { useState } from "react";
+import { useQuery, useMutation } from "@tanstack/react-query";
+import { getModules, createModule, updateModule, updateModuleStatus } from "../services/modules";
+import { useAuth } from "../../../hooks/useAuth";
 
-export const useModules = () => {
+export function useModules({ enabled = true } = {}) {
+    const { auth } = useAuth();
+    const token = auth?.token;
 
-    const [modules, setModules] = useState([])
-    const [loading, setLoading] = useState(false)
-    const [errorModules, setErrorModules] = useState(null)
+    // Estado global para manejo de carga y errores
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-    const getModules = async () => {
-        setLoading(true)
-        setErrorModules(null)
-        try {
-            const newProfiles = await getModulesAPI()
-            setModules(newProfiles)
-        } catch (error) {
-            setErrorModules(error)
-        } finally {
-            setLoading(false)
-        }
+    const mapModules = (module) => ({
+        id_module: module.id_module,
+        name: module.name,
+        key: module.key,
+        status: module.status,
+    });
+
+    // Obtener usuarios
+    const { data: resModules, isFetching: isFetchingModules, error: errorModules, refetch } = useQuery({
+        queryKey: ["modules"],
+        queryFn: async () => {
+            const response = await getModules(token);
+            return response.data ? response.data.map(mapModules) : [];
+        },
+        enabled,
+        retry: 1,
+        refetchOnWindowFocus: false,
+    });
+
+    // Función para manejar estados globales en las mutaciones
+    const handleMutationState = (loading, error = null) => {
+        setIsLoading(loading);
+        setError(error);
     };
 
-    const activateModule = async (id) => {
-        console.log(`Activating profile with ID ${id}`);
-        // Aquí podrías llamar a una API para activar al perfil
-        const updatedModules = modules.map((module) =>
-            module.id === id ? { ...module, status: 1 } : module
-        );
-        setModules(updatedModules);
-    };
 
-    const deactivateModule = async (id) => {
-        console.log(`Deactivating profile with ID ${id}`);
-        const updatedModules = modules.map((module) =>
-            module.id === id ? { ...module, status: 2 } : module
-        );
-        setModules(updatedModules);
-    };
+    // Mutación para crear usuario
+    const post = useMutation({
+        mutationFn: async (moduleData) => createModule(token, moduleData),
+        onMutate: () => handleMutationState(true),
+        onSuccess: () => {
+            handleMutationState(false);
+        },
+        onError: (err) => handleMutationState(false, err)
+    });
 
-    const editModul = (id) => {
-        console.log(`Editing profile with ID ${id}`);
-        // Lógica para abrir un modal o redirigir a una pantalla de edición
-    };
+    // Mutación para editar usuario
+    const edit = useMutation({
+        mutationFn: async ({ id_module, formData }) => updateModule(token, id_module, formData),
+        onMutate: () => handleMutationState(true),
+        onSuccess: () => {
+            handleMutationState(false);
+        },
+        onError: (err) => handleMutationState(false, err),
+    });
 
-    return { modules, loading, errorModules, getModules, activateModule, deactivateModule, editModul }
-}
- 
+    // Mutación para actualizar estado del usuario
+    const updateStatus = useMutation({
+        mutationFn: async ({ id_module, status }) => updateModuleStatus(token, id_module, status),
+        onMutate: () => handleMutationState(true),
+        onSuccess: () => {
+            handleMutationState(false);
+        },
+        onError: (err) => handleMutationState(false, err),
+    });
+
+
+    let loading = isLoading || isFetchingModules;
+    let newError = error || errorModules;
+
+    return {
+        resModules, isLoading: loading, error:newError,refetch,
+        post, edit, updateStatus, handleMutationState
+    };
+};
