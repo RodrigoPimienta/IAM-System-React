cc2fb59f3a96751ca80e762ed3f53fb233b8a50b
RodrigoPimienta <capitanpimienta28@gmail.com>
2025-03-01 19:32:51 -0600

    feat(constants): :sparkles: Add constasnt to modules

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,7 +3,7 @@ import { useRoutes, useNavigate } from 'react-router';
 import { QueryClient } from '@tanstack/react-query';
 import { usePermissions } from './hooks/usePermissions';
 import { PublicLayout, AdminLayout, ProtectedRoute, Login } from './layouts/index';
-import { HomePublic, Users, Profiles, Modules, ModulesRols, ModulesPermissions, HomePrivate, AddUser, UpdateUser } from './pages/index';
+import { HomePublic, Users, AddUser, UpdateUser , Profiles, Modules, AddModule, UpdateModule, PermissionsModule, AddPermissionsModule, UpdatePermissionsModule, HomePrivate} from './pages/index';
 import { PermissionsProvider, ErrorProvider } from './context/index';
 import AccessDenied from './components/AccessDenied'; // Importa AccessDenied
 
@@ -52,12 +52,38 @@ function AppRoutes() {
           path: "users",
           element: <RouteWithProps component={Users} title="Users" moduleKey="users" requiredPermission="show" />,
         },
-        { path: "users/add", element: <AddUser /> },
-        { path: "users/:id_user/update", element: <UpdateUser /> },
+        { path: "users/add",
+          element: <RouteWithProps component={AddUser} title="Add User" moduleKey="users" requiredPermission="create" />
+        },
+        { path: "users/:id_user/update", 
+          element: <RouteWithProps component={UpdateUser} title="Update User" moduleKey="users" requiredPermission="update" />
+        },
+        {
+          path: "modules",
+          element: <RouteWithProps component={Modules} title="Modules" moduleKey="modules" requiredPermission="show" />,
+        },
+        { path: "modules/add",
+          element: <RouteWithProps component={AddModule} title="Add Module" moduleKey="modules" requiredPermission="create" />
+        },
+        { path: "modules/:id_module/update", 
+          element: <RouteWithProps component={UpdateModule} title="Update Module" moduleKey="modules" requiredPermission="update" />
+        },
+        { path: "modules/:id_module/permissions", 
+          element: <RouteWithProps component={PermissionsModule} title="Permissions Module" moduleKey="modules" requiredPermission="showPermissions" />
+        },
+        {
+          path: "modules/:id_module/permissions/add",
+          element: <RouteWithProps component={AddPermissionsModule} title="Add Permission Module" moduleKey="modules" requiredPermission="addPermission" />
+        },
+        { 
+          path: "modules/:id_module/permissions/:id_permission/update",
+          element: <RouteWithProps component={UpdatePermissionsModule} title="Update Permission Module" moduleKey="modules" requiredPermission="updatePermission" />
+        },
+
         { path: "profiles", element: <Profiles /> },
         { path: "modules", element: <Modules /> },
-        { path: "modules/:moduleId/rols", element: <ModulesRols /> },
-        { path: "modules/:moduleId/permissions", element: <ModulesPermissions /> },
+        // { path: "modules/:moduleId/rols", element: <ModulesRols /> },
+        // { path: "modules/:moduleId/permissions", element: <ModulesPermissions /> },
       ],
     },
   ]);
--- src/features/modules/constants/index.js
+++ src/features/modules/constants/index.js
@@ -1,6 +1,16 @@
-const statusMap = {
+const statusMapModules = {
     0: 'Inactive',
     1: 'Active',
 };
 
-export {statusMap};
+const statusMapPermissionsModules = {
+    0: 'Inactive',
+    1: 'Active',
+};
+
+const statusMapRolesModules = {
+    0: 'Inactive',
+    1: 'Active',
+};
+
+export {statusMapModules, statusMapPermissionsModules, statusMapRolesModules};
